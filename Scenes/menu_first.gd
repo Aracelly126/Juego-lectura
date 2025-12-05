deb9ae1e6b8ce3984c2a8a6e4d08a28969a4fdcc
JosliBlue <eliassaulojoslimuyu@hotmail.com>
2025-12-04 23:08:02 -0500

    feat:menu modificado y personaje con camara integrado

--- Scenes/menu_first.gd
+++ Scenes/menu_first.gd
@@ -1,15 +1,6 @@
-extends Node2D
+extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_button_play_pressed() -> void:
+func _on_button_play_pressed():
+	# Esta instrucción le dice al juego que cambie a la escena del nivel 3D.
+	# Según tu imagen, el archivo está dentro de la carpeta "Scenes".
 	get_tree().change_scene_to_file("res://Scenes/first_3d.tscn")
